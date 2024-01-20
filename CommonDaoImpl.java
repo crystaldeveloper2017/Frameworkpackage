c6a545c41012b7885758c870f60add4c59f1ba9d
Your Name <you@example.com>
2024-01-20 10:31:37 +0530

    Changes for reporting framework

--- CommonDaoImpl.java
+++ CommonDaoImpl.java
@@ -1,5 +1,33 @@
 package com.crystal.Frameworkpackage;
 
-public class CommonDaoImpl {
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
 
+public class CommonDaoImpl extends CommonFunctions{
+    public LinkedHashMap<String, String> getReportdetails(String reportId,Connection con) throws Exception
+	{		
+		ArrayList<Object> parameters = new ArrayList<>();
+		String query="select * from report where report_id=?";		
+		parameters.add(reportId);
+		return getMap(parameters, query, con);			
+	}
+
+    public List<LinkedHashMap<String, Object>> getReportParameters(String reportId,Connection con) throws Exception
+	{		
+		ArrayList<Object> parameters = new ArrayList<>();
+		String query="select * from report_parameters where report_id=?";		
+		parameters.add(reportId);
+		return getListOfLinkedHashHashMap(parameters, query, con);			
+	}
+
+    public List<LinkedHashMap<String, Object>> getReportColumns(String reportId,Connection con) throws Exception
+	{		
+		ArrayList<Object> parameters = new ArrayList<>();
+		String query="select * from report_columns where report_id=?";		
+		parameters.add(reportId);
+		return getListOfLinkedHashHashMap(parameters, query, con);			
+	}
+	
 }
